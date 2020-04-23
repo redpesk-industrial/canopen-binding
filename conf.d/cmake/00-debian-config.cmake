44d7e29e22b4d207a29e17cc829e9932e0fe034b
Johann Gautier <johann.gautier@iot.bzh>
2020-04-23 11:14:03 +0200

    clin config files

--- conf.d/cmake/00-debian-config.cmake
+++ conf.d/cmake/00-debian-config.cmake
@@ -1,9 +1,3 @@
 message(STATUS "Custom options: 00-debian-config.cmake --")
 add_definitions(-DSUSE_LUA_INCDIR)
 list(APPEND PKG_REQUIRED_LIST lua53-c++>=5.3)
-
-# liblely-core is not part of standard Linux Distro (https://liblely-core.org/)
-#set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/opt/AGL/lib64/pkgconfig:/opt/liblely-coapp/pkgconfig:")
-
-# Remeber sharelib path to simplify test & debug
-#set(BINDINGS_LINK_FLAG "-Xlinker -rpath=/opt/libmodbus-3.1.6/lib64")
